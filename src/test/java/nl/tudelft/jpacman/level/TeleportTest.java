00dfa2504664972a6c2a73ee8c47186c1bdcb523
Florent Barraco <florent.barraco@student.umons.ac.be>
2016-04-14 16:02:18 +0200

    Nouveaux tests pour la classe Teleport

--- src/test/java/nl/tudelft/jpacman/level/TeleportTest.java
+++ src/test/java/nl/tudelft/jpacman/level/TeleportTest.java
@@ -1,8 +1,11 @@
 package nl.tudelft.jpacman.level;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 import org.junit.rules.ExpectedException;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNull;
 import static org.hamcrest.CoreMatchers.instanceOf;
 
@@ -17,6 +20,7 @@ import nl.tudelft.jpacman.board.Unit;
 import nl.tudelft.jpacman.level.LevelFactory;
 import nl.tudelft.jpacman.level.MapParser;
 import nl.tudelft.jpacman.level.Teleport;
+import nl.tudelft.jpacman.npc.ghost.Ghost;
 import nl.tudelft.jpacman.npc.ghost.GhostFactory;
 import nl.tudelft.jpacman.sprite.PacManSprites;
 import nl.tudelft.jpacman.sprite.Sprite;
@@ -48,19 +52,22 @@ public class TeleportTest {
 	}
 	
 	/**
-	 * Verifies that an 'T' character in a string representing a board produce a Square object only occupied by a Teleport object.
+	 * Verifies that an 'T' character in a string representing a board produce
+	 * a Square object only occupied by a Teleport object that point to the
+	 * square at the coordinate specified in the teleport section of the board string.
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void BoardteleportTest() throws IOException {
+	public void boardteleportTest() throws IOException {
 		PacManSprites sprites = new PacManSprites();
-		MapParser parser = new MapParser(new LevelFactory(sprites, new GhostFactory(sprites)), new BoardFactory(sprites));
-		Board b = parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#T #", "####","----", "2 1 "))).getBoard();
+		MapParser parser = new MapParser(new LevelFactory(sprites,
+				new GhostFactory(sprites)), new BoardFactory(sprites));
+		Board b = parser.parseMap(Lists.newArrayList(Lists.newArrayList("####",
+				"#T #", "####","----", "2 1 "))).getBoard();
 		Square s1 = b.squareAt(1, 1);
 		Square s2 = b.squareAt(2, 1);
 		List<Unit> occupants =  s1.getOccupants();
-
 		assertEquals(occupants.size(), 1);
 		assertThat(occupants.get(0),  instanceOf(Teleport.class));
 		Teleport t = (Teleport) occupants.get(0);
@@ -73,101 +80,235 @@ public class TeleportTest {
 	 * @throws IOException
 	 */
 	@Test
-	public void FomatFailTest1() {
+	public void fomatFailTest1() {
 		PacManSprites sprites = new PacManSprites();
-		MapParser parser = new MapParser(new LevelFactory(sprites, new GhostFactory(sprites)), new BoardFactory(sprites));
+		MapParser parser = new MapParser(new LevelFactory(sprites,
+				new GhostFactory(sprites)), new BoardFactory(sprites));
         thrown.expect(PacmanConfigurationException.class);
-		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#T #", "####","----", "-1 1"))).getBoard();
+		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#T #",
+				"####","----", "-1 1"))).getBoard();
 	}
 	
 	/**
-	 * Verifies that a teleport reference can't be another character
+	 * Verifies that a teleport reference coordinate can't be another string
+	 * than an positive integer.
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void FomatFailTest2() {
+	public void fomatFailTest2() {
 		PacManSprites sprites = new PacManSprites();
-		MapParser parser = new MapParser(new LevelFactory(sprites, new GhostFactory(sprites)), new BoardFactory(sprites));
+		MapParser parser = new MapParser(new LevelFactory(sprites,
+				new GhostFactory(sprites)), new BoardFactory(sprites));
         thrown.expect(PacmanConfigurationException.class);
-		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#T #", "####","----", "1 A "))).getBoard();
+		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#T #",
+				"####","----", "1 A "))).getBoard();
 	}
 	
 	/**
-	 * Verifies that a teleport reference can't contain only one element.
+	 * Verifies that a teleport reference coordinate can't contain
+	 * only one number.
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void FomatFailTest3() {
+	public void fomatFailTest3() {
 		PacManSprites sprites = new PacManSprites();
-		MapParser parser = new MapParser(new LevelFactory(sprites, new GhostFactory(sprites)), new BoardFactory(sprites));
+		MapParser parser = new MapParser(new LevelFactory(sprites,
+				new GhostFactory(sprites)), new BoardFactory(sprites));
         thrown.expect(PacmanConfigurationException.class);
-		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#T #", "####","----", "  2 "))).getBoard();
+		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#T #",
+				"####","----", "  2 "))).getBoard();
 	}
 	
 	/**
-	 * Verifies that a teleport reference can't point out of the board.
+	 * Verifies that a teleport reference coordinate can't point out of the board.
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void FomatFailTest4() {
+	public void fomatFailTest4() {
 		PacManSprites sprites = new PacManSprites();
-		MapParser parser = new MapParser(new LevelFactory(sprites, new GhostFactory(sprites)), new BoardFactory(sprites));
+		MapParser parser = new MapParser(new LevelFactory(sprites,
+				new GhostFactory(sprites)), new BoardFactory(sprites));
         thrown.expect(PacmanConfigurationException.class);
-		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#TP#", "####","----", "3 3 "))).getBoard();
+		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#TP#",
+				"####","----", "3 3 "))).getBoard();
 	}
 	
 	/**
-	 * Verifies that all teleport reference cannot have no references in the case where the reference list is incomplete.
+	 * Verifies that all teleport reference cannot have no references in the
+	 * case where the reference list is incomplete.
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void FomatFailTest5() {
+	public void fomatFailTest5() {
 		PacManSprites sprites = new PacManSprites();
-		MapParser parser = new MapParser(new LevelFactory(sprites, new GhostFactory(sprites)), new BoardFactory(sprites));
+		MapParser parser = new MapParser(new LevelFactory(sprites,
+				new GhostFactory(sprites)), new BoardFactory(sprites));
         thrown.expect(PacmanConfigurationException.class);
-		parser.parseMap(Lists.newArrayList(Lists.newArrayList("#####", "#TPT#", "#####","-----", "1 1  "))).getBoard();
+		parser.parseMap(Lists.newArrayList(Lists.newArrayList("#####", "#TPT#",
+				"#####","-----", "1 1  "))).getBoard();
 	}
 	
 	/**
-	 * Verifies that all teleport reference cannot have no references in the case where the reference list is empty.
+	 * Verifies that all teleport reference cannot have no references in the
+	 * case where the reference list is empty.
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void FomatFailTest6() {
+	public void fomatFailTest6() {
 		PacManSprites sprites = new PacManSprites();
-		MapParser parser = new MapParser(new LevelFactory(sprites, new GhostFactory(sprites)), new BoardFactory(sprites));
+		MapParser parser = new MapParser(new LevelFactory(sprites,
+				new GhostFactory(sprites)), new BoardFactory(sprites));
         thrown.expect(PacmanConfigurationException.class);
-		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#TP#", "####"))).getBoard();
+		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#TP#",
+				"####"))).getBoard();
 	}
 	
 	/**
-	 * Verifies that a teleport reference can't contain more than two element.
+	 * Verifies that a teleport reference coordinate can't contain more than two element.
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void FomatFailTest7() {
+	public void fomatFailTest7() {
 		PacManSprites sprites = new PacManSprites();
-		MapParser parser = new MapParser(new LevelFactory(sprites, new GhostFactory(sprites)), new BoardFactory(sprites));
+		MapParser parser = new MapParser(new LevelFactory(sprites,
+				new GhostFactory(sprites)), new BoardFactory(sprites));
         thrown.expect(PacmanConfigurationException.class);
-		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#T #", "####","----", " 2 3 2 "))).getBoard();
+		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "#T #",
+				"####","----", " 2 3 2 "))).getBoard();
 	}
 	
 	/**
-	 * Verifies that there can't be more references than teleports.
+	 * Verifies that there can't be more teleport reference coordinate than teleports on the board string.
 	 * 
 	 * @throws IOException
 	 */
 	@Test
-	public void FomatFailTest8() {
+	public void fomatFailTest8() {
 		PacManSprites sprites = new PacManSprites();
-		MapParser parser = new MapParser(new LevelFactory(sprites, new GhostFactory(sprites)), new BoardFactory(sprites));
+		MapParser parser = new MapParser(new LevelFactory(sprites,
+				new GhostFactory(sprites)), new BoardFactory(sprites));
         thrown.expect(PacmanConfigurationException.class);
-		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "# P#", "####","----", "2 3 "))).getBoard();
+		parser.parseMap(Lists.newArrayList(Lists.newArrayList("####", "# P#",
+				"####","----", "2 3 "))).getBoard();
+	}
+	
+	/**
+	 * Verifies that a player is really warp to the square pointed by a
+	 * teleport
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void playerWarpTest() {
+		PacManSprites pms = new PacManSprites();
+		MapParser parser = new MapParser(new LevelFactory(pms, new GhostFactory(pms)), new BoardFactory(pms));
+		Board b = parser.parseMap(Lists.newArrayList("######","# T  #", "######", "------", "4 1   ")).getBoard();
+		Square teleportSquare = b.squareAt(2, 1);
+		Square destinationSquare = b.squareAt(4, 1);
+		Player p = new Player(pms.getPacmanSprites(),pms.getPacManDeathAnimation());
+		CollisionMap cm = new PlayerCollisions();
+		Unit teleport = teleportSquare.getOccupants().get(0);
+		p.occupy(teleportSquare);
+		cm.collide(p, teleport);
+		assertEquals(teleportSquare.getOccupants().size(), 1);
+        assertTrue(teleportSquare.getOccupants().get(0) instanceof Teleport);
+		assertEquals(destinationSquare.getOccupants().size(), 1);
+        assertTrue(destinationSquare.getOccupants().get(0) instanceof Player);
+	}
+	
+	/**
+	 * Verifies that a player can't warp to the square pointed by a
+	 * teleport if that square is inaccessible to that player.
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void playerImpossibleWarpTest() {
+		PacManSprites pms = new PacManSprites();
+		MapParser parser = new MapParser(new LevelFactory(pms, new GhostFactory(pms)), new BoardFactory(pms));
+		// Square (4, 1) is a wall, so the player shouldn't be able to be there.
+		Board b = parser.parseMap(Lists.newArrayList("######","# T ##", "######", "------", "4 1   ")).getBoard();
+		Square teleportSquare = b.squareAt(2, 1);
+		Player p = new Player(pms.getPacmanSprites(),pms.getPacManDeathAnimation());
+		CollisionMap cm = new PlayerCollisions();
+		Unit teleport = teleportSquare.getOccupants().get(0);
+		p.occupy(teleportSquare);
+		cm.collide(p, teleport);
+		assertEquals(teleportSquare.getOccupants().size(), 2);
+        assertTrue(teleportSquare.getOccupants().get(0) instanceof Teleport);
+        assertTrue(teleportSquare.getOccupants().get(1) instanceof Player);
+	}
+	
+	/**
+	 * Verifies that a player die if it warp to a square where a ghost is
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void playerWarpAndDieTest() {
+		PacManSprites pms = new PacManSprites();
+		GhostFactory gf = new GhostFactory(pms);
+		MapParser parser = new MapParser(new LevelFactory(pms, gf), new BoardFactory(pms));
+		Board b = parser.parseMap(Lists.newArrayList("######","# T  #", "######", "------", "4 1   ")).getBoard();
+		Square teleportSquare = b.squareAt(2, 1);
+		Square GhostSquare = b.squareAt(4, 1);
+		Player p = new Player(pms.getPacmanSprites(),pms.getPacManDeathAnimation());
+		Ghost g = gf.createBlinky();
+		CollisionMap cm = new PlayerCollisions();
+		Unit teleport = teleportSquare.getOccupants().get(0);
+		p.occupy(teleportSquare);
+		g.occupy(GhostSquare);
+		cm.collide(p, teleport);
+        assertFalse(p.isAlive());
+	}
+	
+	/**
+	 * Verifies that a player is trapped if it warp to a square where a hole is
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void playerWarpAndTrapTest() {
+		PacManSprites pms = new PacManSprites();
+		MapParser parser = new MapParser(new LevelFactory(pms, new GhostFactory(pms)), new BoardFactory(pms));
+		Board b = parser.parseMap(Lists.newArrayList("######","# T H#", "######", "------", "4 1   ")).getBoard();
+		Square teleportSquare = b.squareAt(2, 1);
+		Player p = new Player(pms.getPacmanSprites(),pms.getPacManDeathAnimation());
+		CollisionMap cm = new PlayerCollisions();
+		Unit teleport = teleportSquare.getOccupants().get(0);
+		p.occupy(teleportSquare);
+		cm.collide(p, teleport);
+        assertFalse(p.getMobility());
+	}
+	
+	/**
+	 * Verifies that a player is really warp to the square pointed by a
+	 * teleport
+	 * 
+	 * @throws IOException
+	 */
+	@Test
+	public void playerNoconsecutiveWarpTest() {
+		PacManSprites pms = new PacManSprites();
+		MapParser parser = new MapParser(new LevelFactory(pms, new GhostFactory(pms)), new BoardFactory(pms));
+		Board b = parser.parseMap(Lists.newArrayList("######","# T T#", "######", "------", "4 1   ", "1 1   ")).getBoard();
+		Square teleportSquare = b.squareAt(2, 1);
+		Square SecondTeleportSquare = b.squareAt(4, 1);
+		Square SecondDestinationSquare = b.squareAt(1, 1);
+		Player p = new Player(pms.getPacmanSprites(),pms.getPacManDeathAnimation());
+		CollisionMap cm = new PlayerCollisions();
+		Unit teleport = teleportSquare.getOccupants().get(0);
+		p.occupy(teleportSquare);
+		cm.collide(p, teleport);
+		assertEquals(SecondTeleportSquare.getOccupants().size(), 2);
+	    assertTrue(SecondTeleportSquare.getOccupants().get(0) instanceof Teleport);
+	    assertTrue(SecondTeleportSquare.getOccupants().get(1) instanceof Player);
+		assertEquals(SecondDestinationSquare.getOccupants().size(), 0);
 	}
 }
